27d7342a23aad637ab44bddb591e8d60cdc7d1c7
Romangduck <rrlagusejr@naver.com>
2024-11-12 05:47:22 +0900

    Day - 10

--- src/access/b/AccessOuterMain.java
+++ src/access/b/AccessOuterMain.java
@@ -1,14 +1,16 @@
-package access.a;
+package access.b;
 
-public class AccessInnerMain {
+import access.a.AccessData;
+
+public class AccessOuterMain {
     public static void main(String[] args) {
         AccessData data = new AccessData();
 
         data.publicField = 1;
         data.publicMethod();
 
-        data.defaultField = 2;
-        data.defaultMethod();
+//        data.defaultField = 2;
+//        data.defaultMethod();
 
 //        data.privateField =3;
 //        data.privateMethod();
